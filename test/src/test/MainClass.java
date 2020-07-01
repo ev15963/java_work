bcf48f24dc598c222b8daa3a6f3906f7fc491feb
admin <admin@DESKTOP-9GC530A>
2020-07-01 17:09:51 +0900

    선언부, 출력 17:08

--- test/src/test/MainClass.java
+++ test/src/test/MainClass.java
@@ -13,7 +13,7 @@ public class MainClass {
 		int n;
 //		int n = 10; 중복선언 오류
 		n=10;	//변수사용
-		m=100; //변수 사용전 반드시 선언
+//		m=100; //변수 사용전 반드시 선언
 		int m=100;
 		//float -> 4byte
 		//double -> 8byte
@@ -25,6 +25,11 @@ public class MainClass {
 		char c3='갈';
 		char c4='1';
 		String s1 ="1234";	//string ""로 감싸준다.
+		System.out.print(s1);
+		System.out.println(); //syso CTRL+SPACE
+		System.out.printf("갯수는 %d 개 입니다.", n);
+		//메서드(메소드:method)
+		System.out.print("aaa");
 	}
 
 }
