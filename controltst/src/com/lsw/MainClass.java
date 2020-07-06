8b9f1bd72799357c7cc43a79ad51ea55c8881994
ev15963 <ev15963@hs.ac.kr>
2020-07-06 14:02:38 +0900

    클래스 연습 데이터 준비 14:02

--- controltst/src/com/lsw/MainClass.java
+++ controltst/src/com/lsw/MainClass.java
@@ -10,7 +10,7 @@ public class MainClass {
 
 		DataClass dc = new DataClass();
 		TestClass tc = new TestClass("800211","10221412");
-		System.out.println(tc.age);
+		System.out.println(tc.age); //40
 //		Object o = new Object(); //모든 클래스의 원조
 		System.out.println("main() 종료");
 	}
