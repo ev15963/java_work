0458e429481427dac9397fdcf2ad760b9ed5b9b4
ev15963 <ev15963@hs.ac.kr>
2020-07-06 14:31:03 +0900

    MainClass에 ㅇProfessorClass 선언 14:30

--- porf_prj/src/com/lsw/MainClass.java
+++ porf_prj/src/com/lsw/MainClass.java
@@ -3,8 +3,18 @@ package com.lsw;
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ProfessorClass pc1 = new ProfessorClass("92001", "590327-1839240", "이태규", 920, "교수", "1997");
+		System.out.println(pc1.year);
+		ProfessorClass pc2 = new ProfessorClass("92002", "690702-1350026", "고희석", 910, "부교수", "2003");
+		System.out.println(pc2.year);
+		ProfessorClass pc3 = new ProfessorClass("92002", "690702-1350026", "고희석", 910, "부교수", "2003");
+		System.out.println(pc3.year);
+		ProfessorClass pc4 = new ProfessorClass("92302", "750728-1102458", "김태석", 920, "교수", "1999");
+		System.out.println(pc4.year);
+		ProfessorClass pc5 = new ProfessorClass("92501", "620505-1200546", "박철재", 900, "조교수", "2007");
+		System.out.println(pc5.year);
+		ProfessorClass pc6 = new ProfessorClass("92502", "740101-1830264", "장민석", 910, "부교수", "2005");
+		System.out.println(pc6.year);
 	}
 
 }
--- porf_prj/src/com/lsw/ProfessorClass.java
+++ porf_prj/src/com/lsw/ProfessorClass.java
@@ -2,11 +2,19 @@ package com.lsw;
 
 public class ProfessorClass {
 	//("92001", "590327-1839240", "이태규", 920, "교수", "1997")
-	
+	//이 영역에 선언되는 변수는 클래스으 인스턴스 변수
+	//인스턴스는 클래스가 객체로 생성되는 과정
+	//생성된 객체는 하나의 인스턴스 결과..
 	public String n, jumin, name, rank, year;
 	public int pay;
 	
 	public ProfessorClass(String no, String j, String n, int p, String r, String y) {
 		
+		this.n=no; // 검색속도 빠름 바로 public String가서 찾음
+		this.jumin=j;
+		this.name=n;
+		this.rank=r;
+		this.pay=p;
+		this.year=y;
 	} // 생성자 END
 } // 클래스 END
