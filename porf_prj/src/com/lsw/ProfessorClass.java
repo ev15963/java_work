e1a169ee97fbca7abd56b76082b406f65e6550a3
ev15963 <ev15963@hs.ac.kr>
2020-07-06 14:10:24 +0900

    ProfessorClass 변수 선언 14:10

--- porf_prj/src/com/lsw/ProfessorClass.java
+++ porf_prj/src/com/lsw/ProfessorClass.java
@@ -1,5 +1,12 @@
 package com.lsw;
 
 public class ProfessorClass {
-
+	//("92001", "590327-1839240", "이태규", 920, "교수", "1997")
+	
+	public String n, jumin, name, rank, year;
+	public int pay;
+	
+	public ProfessorClass() {
+		// TODO Auto-generated constructor stub
+	}
 }
