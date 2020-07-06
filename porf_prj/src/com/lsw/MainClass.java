bc981d8fd6d2ee0a5395f50491d9646201f758ad
ev15963 <ev15963@hs.ac.kr>
2020-07-06 16:39:51 +0900

    year를 이용해서 재직년도 구하식 16:39

--- porf_prj/src/com/lsw/MainClass.java
+++ porf_prj/src/com/lsw/MainClass.java
@@ -11,15 +11,23 @@ public class MainClass {
 		profs[4] = new ProfessorClass("92501", "620505-1200546", "박철재", 900, "조교수", "2007");
 		profs[5] = new ProfessorClass("92502", "740101-1830264", "장민석", 910, "부교수", "2005");
 
-		for (int i = 0; i < profs.length; i++) {
-			System.out.println(profs[i].age);
-		}
+		// for (int i = 0; i < profs.length; i++) {
+		// System.out.println(profs[i].age);
+		// }
+
+		profs[5].ageMethod(profs[5].jumin);
+
+		System.out.println(profs[5].age);
 		// int sum = 0;
 		//// sum=pc1.pay+pc2.pay+pc3.pay+pc4.pay+pc5.pay+pc6.pay;
 		// for(int i=0;i<profs.length;i++) {
 		// sum = sum+profs[i].pay;
 		// }
 		// System.out.println("나이 합:"+sum);
+
+		for (int i = 0; i < profs.length; i++) {
+			System.out.println("재직년수는 "+profs[i].jae+"년차입니다.");
+		}
 	}
 
 }
--- porf_prj/src/com/lsw/ProfessorClass.java
+++ porf_prj/src/com/lsw/ProfessorClass.java
@@ -1,27 +1,29 @@
 package com.lsw;
 
 public class ProfessorClass {
-	//("92001", "590327-1839240", "이태규", 920, "교수", "1997")
-	//이 영역에 선언되는 변수는 클래스으 인스턴스 변수
-	//인스턴스는 클래스가 객체로 생성되는 과정
-	//생성된 객체는 하나의 인스턴스 결과..
+	// ("92001", "590327-1839240", "이태규", 920, "교수", "1997")
+	// 이 영역에 선언되는 변수는 클래스으 인스턴스 변수
+	// 인스턴스는 클래스가 객체로 생성되는 과정
+	// 생성된 객체는 하나의 인스턴스 결과..
 	public String n, jumin, name, rank, year;
 	public int pay;
-	public int age=0;
-	
+	public int age = 0;
+	public int jae = 0;
+
 	public ProfessorClass(String no, String j, String n, int p, String r, String y) {
-	
-		this.n=no; // 검색속도 빠름 바로 public String가서 찾음
-		this.jumin=j;
-		this.name=n;
-		this.rank=r;
-		this.pay=p;
-		this.year=y;
-		
-		this.ageMethod(this.jumin);
+
+		this.n = no; // 검색속도 빠름 바로 public String가서 찾음
+		this.jumin = j;
+		this.name = n;
+		this.rank = r;
+		this.pay = p;
+		this.year = y; // 입사년도 -> 재직년수
+
+		this.ageMethod(this.jumin); // 객체생성이 끝날때 age 값을 생성
+		this.jaeMethod(this.year);
 	} // 생성자 END
-	
-	//주민번호를 이용한 나이계산 메소드
+
+	// 주민번호를 이용한 나이계산 메소드
 	public void ageMethod(String jumin) {
 		String[] ju=jumin.split("-");	//"590327-1839240" 분리
 		
@@ -35,6 +37,7 @@ public class ProfessorClass {
 			this.age=2020-(temp+2000); //2000 년대 나이 구하기
 		}
 		
+		
 //		public void age(String s, String s1) { // s : String n1
 //			String nfront=s.substring(0,2); //주민 앞자리중 맨처음 2자리
 //			int temp=Integer.parseInt(nfront);
@@ -46,5 +49,11 @@ public class ProfessorClass {
 //				age=2020-(temp+2020);
 //			}
 //		}
-	} //ageMethod() END
+	} // ageMethod() END
+	
+	public void jaeMethod(String year) {
+		
+		int y = Integer.parseInt(year);
+		this.jae= 2020-y+1;
+	} // jaeMethod() END
 } // 클래스 END
