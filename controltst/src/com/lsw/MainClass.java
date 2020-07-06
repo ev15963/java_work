428f98d1103a75e628d68f0da78fc9831c86f1f3
ev15963 <ev15963@hs.ac.kr>
2020-07-06 12:35:48 +0900

    주민번호를 이용한 나이를 뽑는 방법 12:35

--- controltst/src/com/lsw/MainClass.java
+++ controltst/src/com/lsw/MainClass.java
@@ -13,6 +13,7 @@ public class MainClass {
 		System.out.println(tc.age);
 //		Object o = new Object(); //모든 클래스의 원조
 		System.out.println("main() 종료");
+		System.out.println(year);
 	}
 
 }
--- controltst/src/com/lsw/TestClass.java
+++ controltst/src/com/lsw/TestClass.java
@@ -1,24 +1,41 @@
 package com.lsw;
 
-public class TestClass { //MainClass와 동일위치
-	float f=110.2f;
-	int d=11;
-	//		0123456
-	String n1="770405";//주민 앞번호
-	String n2="1202123";
-	int age=0; //나이 => String.substring(0,2)
-				//  => Integer.parseInt("12")
-	public TestClass(String string) {
+public class TestClass { // MainClass와 동일위치
+	float f = 110.2f;
+	int d = 11;
+	// 0123456
+	String n1 = "770405";// 주민 앞번호
+	String n2 = "1202123";
+	int age = 0; // 나이 => String.substring(0,2)
+					// => Integer.parseInt("12")
+
+	public TestClass(String string1, String string2) {
 		System.out.println("TestClass 생성자 시작");
-		age(string);
+		age(string1, string2);
 		System.out.println("TestClass 생성자 종료");
 	}
-	public void age(String s) { // s : String n1
-		System.out.println("age()에 값 전달:"+s);
-		String n=s.substring(0,2); // n: "77"
-		System.out.println("연도만 추출:"+n);
-		age =Integer.parseInt(n); // a: 77
-		System.out.println("문자열을 숫자로: "+n);
-		System.out.println("age() 종료");
+
+	public void age(String s, String s1) { // s : String n1
+		String nfront=s.substring(0,2); //주민 앞자리중 맨처음 2자리
+		int temp=Integer.parseInt(nfront);
+		
+		String nback=s1.substring(0,1); //주민 뒷자리중 맨처음 1자리
+		if(nback.equals("1")||nback.equals("2")) {
+			age=2020-(temp+1900);
+		} else {
+			age=2020-(temp+2020);
+		}
 	}
+
+//	public int year(int year) {
+//		if (n2.equals("1") || n2.equals("2")) {
+//			year = age + 1900;
+//		} else {
+//			year = age + 2000;
+//		}
+//		return year;
+	}
+	
+	
+	
 }
