85e5dfef68058b711545196d4e77bc9507417e73
ev15963 <ev15963@hs.ac.kr>
2020-07-07 10:35:17 +0900

    startsWidth 10:35

--- book_prj/src/com/lsw/MainClass.java
+++ book_prj/src/com/lsw/MainClass.java
@@ -5,8 +5,11 @@ public class MainClass {
 		DataClass dc=new DataClass(); //BookClass[] books
 		//dc.books[0]=new BookClass("DO it HTML 5 CSS 3", "고경희", "이지스퍼블리싱", 16800, "06365234.jpg", 20);
 		
-		for(int i=dc.books.length-1;i>-1;i--) //역순출력
-			System.out.println(dc.books[i].author);
+		String name="고";
+		
+		for(int i=0;i<dc.books.length;i++) //역순출력
+			if(dc.books[i].author.startsWith(name))
+				System.out.println(dc.books[i].title);
 
 
 	}
