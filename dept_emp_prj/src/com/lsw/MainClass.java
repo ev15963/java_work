2a62e1b1f2ef8e0fe573c42c305e556c592d6b28
ev15963 <ev15963@hs.ac.kr>
2020-07-07 18:59:58 +0900

    특정부서에 근무하는 사람의 이름 출력 18:04

--- dept_emp_prj/src/com/lsw/MainClass.java
+++ dept_emp_prj/src/com/lsw/MainClass.java
@@ -17,21 +17,26 @@ public class MainClass {
 		// System.out.println(DataTestClass.n2[0]);
 
 		// 특정부서에 근무하는 사람의 이름 출력
-		String dName = "경리부";
-		String dNo="";
+		
 		DataClass.initDEPT();
 		DataClass.initEMP();
-		for (int i = 0; i < DataClass.dept.length; i++) { // *
-			if (DataClass.dept[i].deptName.equals(dName)) {
+		String dName = "경리부";
+		String dNo="";
+		for (int i=0;i<DataClass.dept[i].deptName.length();i++) {
+			if(DataClass.dept[i].deptName.equals(dName)) {
 				dNo=DataClass.dept[i].deptNo;
+			//	System.out.println(i); //0
 			}
 		}
-		for (int i=0;i<DataClass.emps.length;i++) {
-			if (DataClass.emps[i].deptNo.equals(dNo)) {
+		
+		for (int i=0;i<DataClass.emps[i].deptNo.length();i++) {
+			if(DataClass.emps[i].deptNo.equals(dNo)) {
 				System.out.println(DataClass.emps[i].empName);
 			}
 		}
 
+		//System.out.println(DataClass.emps[0].empName); //20
 	}
+	
 
 }
