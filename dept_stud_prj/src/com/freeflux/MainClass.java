458ab3b3e2f780eae26f2efd647d11ece2c86269
ev15963 <ev15963@hs.ac.kr>
2020-07-08 14:47:59 +0900

    1. 학생정보 ㅂ 배열(students)로 부터 여학생 검색 14:47

--- dept_stud_prj/src/com/freeflux/MainClass.java
+++ dept_stud_prj/src/com/freeflux/MainClass.java
@@ -4,9 +4,16 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		DataClass d=MethodClass.createObject();
-		System.out.println(d.depts[2].office);
-		System.out.println(d.students[7].gender);
+
+		/**여학생의 학과명(Department), 이름, 평균 나이 구하기**/
+		// 0. 검색 성별 설정
+		char searchGender='F';
 		
+		/** 1. 학생정보 배열(students)로부터 여학생 검색 메서드 호출**/
+		String[] result = MethodClass.searchInfo(searchGender, d.students);
+		for(int idx=0;idx<result.length;idx++) {
+			System.out.println(result[idx]);
+		}
 	} // main() END
 	
 	
--- dept_stud_prj/src/com/freeflux/MethodClass.java
+++ dept_stud_prj/src/com/freeflux/MethodClass.java
@@ -2,20 +2,32 @@ package com.freeflux;
 
 public class MethodClass {
 	public MethodClass() {
-		
+
 	}
-	
-	/**0. 학과 및 학생정보들을 보유하는 클래스 객체 생성 메서드 호출**/
+
+	/** 0. 학과 및 학생정보들을 보유하는 클래스 객체 생성 메서드 호출 **/
 	public static DataClass createObject() {
-		DataClass dc=new DataClass(); 
-		dc.initDepartments();	// 학과정보 배열에 초기 데이터 대입
-		dc.initStudents();	// 학생정보 배열에 초기 데이터 대입
+		DataClass dc = new DataClass();
+		dc.initDepartments(); // 학과정보 배열에 초기 데이터 대입
+		dc.initStudents(); // 학생정보 배열에 초기 데이터 대입
 		return dc;
 	}
-	
-	/**1. */
-	
-	
-	
-	
+
+	/** 1. 학생정보 배열(students)로부터 여학생 검색 메서드 **/
+	public static String searchInfo(char c, Student[] s) {
+		String names = ""; // 검색 학생이름들 : "이름,이름...."
+		String dept_id = ""; // 검색 학생 학과번호들 : "학과번호,학과번호...."
+		int ageSum = 0; // 나이 합
+		int count = 0; // 검색 인원수
+		for (int idx = 0; idx < s.length; idx++) {
+			if (s[idx].gender == c) {
+				names = names + s[idx].name + ","; // 학생 이름 누적
+				dept_id = dept_id + s[idx].dept_id + ","; // 학생 학과번호 누적
+				ageSum = ageSum + s[idx].age; // 학생 나이 누적
+				count = count + 1; // count++ 와 동일 // 인원수 증가
+			}
+		}
+		String[] resultData= {names, dept_id, (ageSum+""), (count+"") };
+		return resultData; //void -> String으로 변환
+	}
 }
