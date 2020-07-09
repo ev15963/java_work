8c8e42aa76659bf01367441c34cd535d0eab46e4
ev15963 <ev15963@hs.ac.kr>
2020-07-09 15:42:22 +0900

    ArrayList<Interger> 숫자만 입력가능 설정 15:42

--- arrayList_1/src/com/lsw/MainClass.java
+++ arrayList_1/src/com/lsw/MainClass.java
@@ -6,20 +6,21 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		/**ArrayList.class**/
-		ArrayList a= new ArrayList();
+		ArrayList<Integer> a= new ArrayList<Integer>();
 		System.out.println("크기:"+a.size());
 		a.add(1);		//index:0
 		a.add("문자열");	 //index:1
 		a.add(true); 	//index:2
 		a.add('A');		//index:3
-		a.add(10.455);	//index:4
+		boolean b=a.add(10.455);	//index:4
 		System.out.println("크기:"+a.size());
 		System.out.println(a.get(1));
 		a.set(1,"새로운 문자열");
 		System.out.println(a.get(1));
-		a.remove(2);
+		boolean b1 = (boolean) a.remove(1); //1
 		System.out.println(a.get(2));
 		System.out.println(a.size());
+		int n= a.size();
 	}
 
 }
