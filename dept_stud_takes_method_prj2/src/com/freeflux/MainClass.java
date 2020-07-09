7c8d057a7f240e9485c3c086ff16d7907aed627e
ev15963 <ev15963@hs.ac.kr>
2020-07-09 10:49:50 +0900

    1. 학점 A이상, 2. r. 검색된 학법으로 학생들의 이름 학과번호 검색 10:49

--- dept_stud_takes_method_prj2/src/com/freeflux/MainClass.java
+++ dept_stud_takes_method_prj2/src/com/freeflux/MainClass.java
@@ -9,10 +9,11 @@ public class MainClass {
 //		System.out.println(MethodClass.dc.takes.length); //출력되는지 확인
 		/** 학점이 A이상(A, A+) 인 학생들의 이름과 학번, 학과명을 출력 **/
 		/** 1. 학점이 A이상(A, A+) 인 학생들의 학번 검색 메서드 호출 **/
-		
-		
+//		MethodClass.searchStudentID("A");
+		String stu_id=MethodClass.searchStudentID("A");
+//		System.out.println(stu_id);
 		/** 2. 검색된 학번을 이용하여 학생들의 이름과 학과번호 검색 메서드 호출 **/
-		
+		MethodClass.searchStudentInfo(stu_id);
 		/** 3. 검색된 학번을 이용하여 학과이름 검색 메서드 호출 **/
 		
 		/** 4. 각 결과 값을 이용하여 결과 출력 메서드 호출 **/
--- dept_stud_takes_method_prj2/src/com/freeflux/MethodClass.java
+++ dept_stud_takes_method_prj2/src/com/freeflux/MethodClass.java
@@ -11,16 +11,36 @@ public class MethodClass {
 		dc= new DataClass();
 	} // createObject() END
 	
-	/** 1. 학점이 A이상(A, A+) 인 학생들의 학번 검색 메서드 호출 **/
+	/** 1. 학점이 A이상(A, A+) 인 학생들의 학번 검색 메서드 호출 
+	 * @return **/
 	// DataClass객체의 takes배열 데이터 중  take 변수 값을 비교하며 검색
-	public static void searchStudentID(String t) {
-
-	}
+	public static String searchStudentID(String t) {
+		dc.initTakes();
+		String temp="";
+//		System.out.println(dc.takes[10].take);
+		for (int i =0; i<dc.takes.length;i++) {
+			if(dc.takes[i].take.startsWith(t)); //if안에 변수 넣으면 초기화
+				temp=temp+dc.takes[i].stu_id+",";
+		} //for() END : int i 소멸
+		return temp;
+	} //searchStudentID() END : String t / String temp 소멸
 	
 	/** 2. 검색된 학번을 이용하여 학생들의 이름과 학과번호 검색 메서드 호출 **/
-	public static void searchStudentInfo() {
+	public static void searchStudentInfo(String stu_id) { //"1292001, ..."
+		dc.initStudents();
+//		System.out.println(dc.students[7].stu_id);
+		String[] temp= {"",""}; //temp[0]:이름들/ temp[1]:학과번호들
+		String[] s_ids=stu_id.split(","); //"1292001" "1292001"
 		
-	}
+		for(int i=0;i<dc.students.length;i++) {
+			if(dc.students[i].stu_id.equals(s_ids[0])) {
+				temp[0]=temp[0]+dc.students[i].name+",";
+				temp[1]=temp[1]+dc.students[i].dept_id+",";
+			}
+		}
+		System.out.println(temp[0]);
+		System.out.println(temp[1]);
+	} //searchStudentInfo() END
 	/** 3. 검색된 학번을 이용하여 학과이름 검색 메서드 호출 **/
 	public static void convertDepartmentID() {
 		
