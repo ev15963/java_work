8f4549f943d8092bae9294e93d474ca4d8dddbab
ev15963 <ev15963@hs.ac.kr>
2020-07-09 16:12:43 +0900

    arraylist 기본메소드 16:12

--- arrayList_1/src/com/lsw/MainClass.java
+++ arrayList_1/src/com/lsw/MainClass.java
@@ -5,22 +5,38 @@ import java.util.ArrayList;
 public class MainClass {
 
 	public static void main(String[] args) {
-		/**ArrayList.class**/
-		ArrayList<Integer> a= new ArrayList<Integer>();
-		System.out.println("크기:"+a.size());
-		a.add(1);		//index:0
-		a.add("문자열");	 //index:1
-		a.add(true); 	//index:2
-		a.add('A');		//index:3
-		boolean b=a.add(10.455);	//index:4
-		System.out.println("크기:"+a.size());
-		System.out.println(a.get(1));
-		a.set(1,"새로운 문자열");
-		System.out.println(a.get(1));
-		boolean b1 = (boolean) a.remove(1); //1
-		System.out.println(a.get(2));
-		System.out.println(a.size());
-		int n= a.size();
-	}
+		/** ArrayList.class **/
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		System.out.println("크기:" + a.size());
+		// a.add(1); //index:0
+		// a.add("문자열"); //index:1
+		// a.add(true); //index:2
+		// a.add('A'); //index:3
+		// boolean b=a.add(10.455); //index:4
+		// System.out.println("크기:"+a.size());
+		// System.out.println(a.get(1));
+		// a.set(1,"새로운 문자열");
+		// System.out.println(a.get(1));
+		// boolean b1 = (boolean) a.remove(1); //1
+		// System.out.println(a.get(2));
+		// System.out.println(a.size());
+		// int n= a.size();
+		a.add(1); // index 0
+		// get(index
 
+		System.out.println("크기 : " + a.size());
+		for (int n = 1; n < 11; n++) {
+			a.add(n);
+			// }
+			// for(int n = 1;n<10;n++) {
+			// System.out.println(a.remove(n));
+			// }
+			// for(int n = 0;n<10;n++) {
+			// System.out.println(a.remove(0));
+			// }
+		}
+			a.clear();
+			System.out.println(a.size());
+
+	}
 }
