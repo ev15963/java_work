0179a65455c591271de79bf6232650dff15a2c19
ev15963 <ev15963@hs.ac.kr>
2020-07-13 15:22:00 +0900

    이미지 추가15:21

--- file_exception/src/com/lsw/FileWriteClass.java
+++ file_exception/src/com/lsw/FileWriteClass.java
@@ -1,5 +1,32 @@
 package com.lsw;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileWriteClass {
 
-}
+	String uri="c:/filetest/java.html";
+	public FileWriteClass() {
+		
+	}
+	FileWriter fw=null;
+	/**지정한 파일에 내용 적기**/
+	public void writeFile() {
+		try {
+			fw=new FileWriter(uri);
+			System.out.println("파일쓰기 준비 완료");
+			fw.write("<!doctype html>");
+			fw.write("<html>");
+			fw.write("<head><title>java => html </title></head>");
+			fw.write("<body>");
+			fw.write("<img src='./kart.JPG' />");
+			fw.write("<a href='https://www.naver.com'>네이버</a>");
+			fw.write("</body>");
+			fw.write("</html>");
+			System.out.println("파일 쓰기 완료");
+			fw.close();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}//writeFile() END
+}//class END
--- file_exception/src/com/lsw/MainClass.java
+++ file_exception/src/com/lsw/MainClass.java
@@ -4,8 +4,9 @@ import java.io.FileNotFoundException;
 
 public class MainClass {
 	public static void main(String[] args) {
-		FileReadClass f1 = new FileReadClass();
-		f1.readFile();
-
+//		FileReadClass f1 = new FileReadClass();
+//		f1.readFile();
+		FileWriteClass f2 = new FileWriteClass();
+		f2.writeFile();
 	}
 }
