cc385518c1706375d481c418a7cf76322526a0a8
ev15963 <ev15963@hs.ac.kr>
2020-07-13 09:32:58 +0900

    Parent.java 내용 추가 09:32

--- file_exception/src/com/lsw/Parent.java
+++ file_exception/src/com/lsw/Parent.java
@@ -1,7 +1,23 @@
 package com.lsw;
 
 public class Parent {
-	public Parent() {
+		public int a = 10;
+		int b = 20;
+		private int c = 30;
+		protected int d=40;
 		
-	}
+		public Parent() {
+		} //»ý¼ºÀÚ END
+		
+		public void m1() {
+			
+		}
+		void m2() {
+		}
+		private void m3() {
+			
+		}
+		protected void m4() {
+			
+		} //class END
 }
