2a001400f4bf3f1c2f08853c85fd68c277edb0fd
ev15963 <ev15963@hs.ac.kr>
2020-07-13 11:18:06 +0900

    파일 읽기 11:18

--- file_exception/src/com/lsw/FileReadClass.java
+++ file_exception/src/com/lsw/FileReadClass.java
@@ -2,4 +2,16 @@ package com.lsw;
 
 public class FileReadClass {
 
+	public FileReadClass() {
+		
+	}//생성자 END
+	
+	/**c://filetest의 memo.txt 파일을 읽는 메서드**/
+	public void readFile() {
+		//1.파일 경로명 설정
+		
+		//2.경로명을 이용하여 해당 파일 읽기
+		//FileReader f=new FileReader("경로명");
+		
+	}
 }
