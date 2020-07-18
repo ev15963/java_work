eb5689a84548478054b640b1f006e62857be68fe
ev15963 <ev15963@hs.ac.kr>
2020-07-19 00:33:27 +0900

    git 재설정 및 MainClass 작성 12:33

--- java_excel_html_bank_pratice/src/com/freeflux/MainClass.java
+++ java_excel_html_bank_pratice/src/com/freeflux/MainClass.java
@@ -3,7 +3,10 @@ package com.freeflux;
 public class MainClass {
 
 	public static void main(String[] args) { //fromconsole createtags makehtml
-//		ObjectClass.
+		String htmlTags=MethodClass.createTags();
+		System.out.println(htmlTags);
+		
+		PrintClass.makeHTML(htmlTags, 1);
 
 	}
 
