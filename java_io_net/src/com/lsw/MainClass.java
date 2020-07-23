7ca98efa4cedb66d0d763eb4bebf2b00e9231c57
ev15963 <ev15963@hs.ac.kr>
2020-07-23 15:38:39 +0900

    printpage()로 네이버 추출 15:38

--- java_io_net/src/com/lsw/MainClass.java
+++ java_io_net/src/com/lsw/MainClass.java
@@ -5,19 +5,28 @@ import java.net.InetAddress;
 public class MainClass {
 
 	public static void main(String[] args) {
-		InetAddress my;
-		try {
-			my = InetAddress.getLocalHost();
-			MyInetAddress inet = new MyInetAddress(my);
-			inet.printInfo();
-
-			// www.naver.com
-			InetAddress naver = InetAddress.getByName("www.naver.com");
-			inet.setInetAddress(naver);
-			inet.printInfo();
-		} catch (java.net.UnknownHostException e) {
-			e.printStackTrace();
-		}
+		String site="http://www.naver.com/index.html";
+		
+		URLReader homePage = new URLReader(site);
+//		homePage.printConnectioninfo();
+		
+		System.out.println("===========================");
+		
+		homePage.printPage();
+		
+//		InetAddress my;
+//		try {
+//			my = InetAddress.getLocalHost();
+//			MyInetAddress inet = new MyInetAddress(my);
+//			inet.printInfo();
+//
+//			// www.naver.com
+//			InetAddress naver = InetAddress.getByName("www.naver.com");
+//			inet.setInetAddress(naver);
+//			inet.printInfo();
+//		} catch (java.net.UnknownHostException e) {
+//			e.printStackTrace();
+//		}
 
 	}
 
