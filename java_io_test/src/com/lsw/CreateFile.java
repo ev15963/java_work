a6efaed345ec9bdc377e10f3cc7ed645e37f69d0
ev15963 <ev15963@hs.ac.kr>
2020-07-23 11:30:49 +0900

    java_io_test 출력 (한글깨짐) 11:30

--- java_io_test/src/com/lsw/CreateFile.java
+++ java_io_test/src/com/lsw/CreateFile.java
@@ -1,9 +1,36 @@
 package com.lsw;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class CreateFile {
 	public static void main(String[] args) {
-		
+		// 읽을 파일 이름 지정
+		String fname = "src/com/lsw/newfiles.txt"; // 핸져 파일 없는 상태
+		// File 객체 생성
+		File f = new File(fname);
+
+		// 실제 newfile.txt. 파일 존재 여부 확인
+		if (!f.exists()) { //! not
+			System.out.println("다음 파일을 찾을 수 없습니다.:" + fname);
+		}
+		// 새로이 파일을 만들어 문자열 쓰기
+		try {
+			f.createNewFile(); //File 객체의 파일 생성 메서드
+			if (f.exists()) { //File 객체.exist()는 존재(true)
+				System.out.println("다음 파일이 없습니다.:" + fname);
+			}
+
+			FileWriter fr;
+			fr = new FileWriter(f);
+			fr.write("파일 내용이 들어갑니다.");
+			fr.close();
+		} catch (IOException e) {
+			System.out.println(e);
+		}
 	}
+
 	public CreateFile() {
 		// TODO Auto-generated constructor stub
 	}
--- java_io_test/src/com/lsw/ReadFiles.java
+++ java_io_test/src/com/lsw/ReadFiles.java
@@ -1,8 +1,30 @@
 package com.lsw;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class ReadFiles {
 	public static void main(String[] args) {
+		int data;
+		//읽을 파일 이름 지정
+		String fname="src/com/lsw/ReadFiles.java";
 		
+		try {
+			//읽을 파일 이름으로 FileInputStream 생성
+			FileInputStream fis = new FileInputStream(fname);
+			
+			//한 바이트씩(유니코드값) 읽어 문자로 변환하여 출력
+			while((data=fis.read()) != -1) {
+				char c = (char) data;
+				System.out.println(c);
+			}
+		} catch (FileNotFoundException e) {
+			System.err.println("다음 파일이 없습니다:"+fname);
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+		System.out.println();
 	}
 	public ReadFiles() {
 		// TODO Auto-generated constructor stub
