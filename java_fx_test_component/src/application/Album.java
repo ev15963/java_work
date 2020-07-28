0b033b3edeedfd50d3fd5145e4d574ddb090666d
ev15963 <ev15963@hs.ac.kr>
2020-07-28 12:15:41 +0900

    album.java 수정 12:15

--- java_fx_test_component/src/application/Album.java
+++ java_fx_test_component/src/application/Album.java
@@ -15,4 +15,30 @@ public class Album {
 		this.release=release;
 	}
 
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	public int getRelease() {
+		return release;
+	}
+
+	public void setRelease(int release) {
+		this.release = release;
+	}
+	
+	
+
 }
