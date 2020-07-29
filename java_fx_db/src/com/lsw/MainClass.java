f24007ed403129d6d92a54478fb0738de9257e7f
ev15963 <ev15963@hs.ac.kr>
2020-07-29 10:05:25 +0900

    root node 객체생성 및 기본설정 + Pagination 객체 생성 10:05

--- java_fx_db/src/com/lsw/MainClass.java
+++ java_fx_db/src/com/lsw/MainClass.java
@@ -1,9 +1,17 @@
 package com.lsw;
 
+import com.lsw.ui.MainView;
+
+import javafx.application.Application;
+
 public class MainClass {
 
 	public static void main(String[] args) {
-		
+		showMain();
 	}
+	public static void showMain() {
 
+		/**window 화면 호출**/
+		Application.launch(MainView.class);
+	}
 }
--- java_fx_db/src/com/lsw/ui/MainView.java
+++ java_fx_db/src/com/lsw/ui/MainView.java
@@ -1,9 +1,36 @@
 package com.lsw.ui;
 
-public class MainView {
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Pagination;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainView extends Application{
 
 	public MainView() {
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception { //생성자
+		/** Root Node 객체생성 및 기본설정*/
+		BorderPane load = new BorderPane(); //상하좌우
+		load.setPadding(new Insets(10));
+		
+		//scene에 최종 레이아웃을 전달
+		//최종 레이아웃 : 가장 바깥 쪽 레이아웃
+		Scene scence = new Scene(load, 600, 400);
+		stage.setScene(scence);
+		//////////////////////////////////////////////여기까지 기본화면 
+		
+		/** Pagination 객체 생성 */
+		Pagination pagination = new Pagination(4); //페이지수
+		load.setCenter(pagination);
+		
+		stage.show();
+		
 	}
 
 }
