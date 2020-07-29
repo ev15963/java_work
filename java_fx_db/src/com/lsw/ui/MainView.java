4f0868649d18a377d4488a908c0334f94f58063a
ev15963 <ev15963@hs.ac.kr>
2020-07-29 11:25:03 +0900

    databaseclass 기본틀 11:24

--- java_fx_db/src/com/lsw/ui/MainView.java
+++ java_fx_db/src/com/lsw/ui/MainView.java
@@ -1,74 +1,108 @@
 package com.lsw.ui;
 
-
+import application.Album;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Pagination;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 
-
-public class MainView extends Application{
+public class MainView extends Application {
 
 	public MainView() {
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public void start(Stage stage) throws Exception { //생성자
-		/** Root Node 객체생성 및 기본설정*/
-		BorderPane load = new BorderPane(); //상하좌우
+	public void start(Stage stage) throws Exception { // 생성자
+		/** Root Node 객체생성 및 기본설정 */
+		BorderPane load = new BorderPane(); // 상하좌우
 		load.setPadding(new Insets(10));
-		
-		//scene에 최종 레이아웃을 전달
-		//최종 레이아웃 : 가장 바깥 쪽 레이아웃
+
+		// scene에 최종 레이아웃을 전달
+		// 최종 레이아웃 : 가장 바깥 쪽 레이아웃
 		Scene scence = new Scene(load, 600, 400);
 		stage.setScene(scence);
-		//////////////////////////////////////////////여기까지 기본화면 
-		
+		////////////////////////////////////////////// 여기까지 기본화면
+
 		/** Pagination 객체 생성 */
-		Pagination pagination = new Pagination(4); //페이지수
+		Pagination pagination = new Pagination(4); // 페이지수
 		load.setCenter(pagination);
-		
-		
-		/**Pagination 스타일 변경 - 페이지 버튼 컨트롤 디자인 변경 */
+
+		/** Pagination 스타일 변경 - 페이지 버튼 컨트롤 디자인 변경 */
 		pagination.getStyleClass().add(Pagination.STYLE_CLASS_BULLET);
-		
-		
-		/**각 버튼을 클릭하거나 좌우 화살표 버튼을 클릭했을 때 */
-		/**Pagination 페이지별 화면구성을 위한 pageFactory 속성 변경 */
+
+		/** 각 버튼을 클릭하거나 좌우 화살표 버튼을 클릭했을 때 */
+		/** Pagination 페이지별 화면구성을 위한 pageFactory 속성 변경 */
 		pagination.setPageFactory(new Callback<Integer, Node>() {
 
 			@Override
 			public Node call(Integer param) {
-				/**인덱스별 페이지 로직처리*/
-				//반환할 레이아웃 객체
+				/** 인덱스별 페이지 로직처리 */
+				// 반환할 레이아웃 객체
 				BorderPane p = new BorderPane();
-				if(param==0) {
-					//기본화면
-				}else if(param==1) {
-					/** 인덱스가 1일 경우의 처리*/
+				if (param == 0) {
+					/** 인덱스가 0일 경우의 페이지 처리 로직 구간 */
+					TableView table = new TableView();
+					TableColumn titleColum = new TableColumn("Title");
+					titleColum.setMinWidth(100); // 컬럼의 최소 크기
+					table.getColumns().add(titleColum);
+
+					TableColumn artistColumn = new TableColumn("Artist");
+					artistColumn.setMinWidth(150);
+					table.getColumns().add(artistColumn);
+
+					TableColumn releaseColumn = new TableColumn("Release Year");
+					releaseColumn.setMinWidth(170);
+					table.getColumns().add(releaseColumn);
+
+					p.setBottom(table);
+					
+				} else if (param == 1) {
+					/** 인덱스가 1일 경우의 처리 */
 					p.setStyle("-fx-background-color:pink");
-					
+
 					Button b = new Button("테스트2");
-					b.setPrefSize(100, 30); //버튼 크기
+					b.setPrefSize(100, 30); // 버튼 크기
 					p.setBottom(b);
-				}else if(param==2) {
-					
+				} else if (param == 2) {
+					/** 인덱스가 2일 경우 페이지 로직 처리 */
+					p.setStyle("-fx-background-color:orage");
+
+					TextField text = new TextField("테스트3");
+					text.setPrefSize(100, 30);
+					p.setBottom(text);
+
 				} else {
-					
+					/** 인덱스가 3일 경우 페이지 로직 처리 */
+					p.setStyle("-fx-background-color:cyan");
+					HBox hbox = new HBox();
+					TextField text1 = new TextField("테스트4");
+					text1.setPrefSize(100, 30);
+					TextField text2 = new TextField("테스트4");
+					text2.setPrefSize(100, 30);
+
+					hbox.getChildren().addAll(text1, text2);
+					p.setBottom(hbox);
 				}
 				return p;
 			}
 		});
-		
-		
+
 		stage.show();
-		
+
 	}
 
 }
---
+++ java_io_test/src/com/lsw/DataBaseClass.java
@@ -0,0 +1,31 @@
+package com.lsw;
+
+public class DataBaseClass {
+
+	/**database 관련 상수 선언 */
+	
+	/**database 관련 객체 선언 */
+	
+	/**그 외 필요한 객체, 변수 선언 */
+
+	
+	public DataBaseClass() { //기본 생성자
+		// TODO Auto-generated constructor stub
+	}
+	
+	/** 1. mysql server에 접속하는 메서드 선언 **/
+	public void connect_db() {
+		
+	} //connect_db() END
+	
+	/** 2. table의 전체 레코드 조회 메서드 선언 **/
+	public void selectAll() {
+		
+	} //selectAll() END
+	
+	/** 각 자원들을 해체하는 메서드 */
+	public void userClose() {
+		
+	} //userClose() END
+
+}
