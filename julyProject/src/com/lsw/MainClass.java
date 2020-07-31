e5e950107b78d20ec054176bb6b284604eff51e9
ev15963 <ev15963@hs.ac.kr>
2020-07-31 11:35:36 +0900

    프로젝트 수정 11111 :35

--- julyProject/src/com/lsw/MainClass.java
+++ julyProject/src/com/lsw/MainClass.java
@@ -1,11 +1,8 @@
 package com.lsw;
 
-import java.util.Scanner;
-
 public class MainClass {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		DataBaseClass dbc = new DataBaseClass();
 		dbc.connect_companydb("company_db");
 		dbc.search();
